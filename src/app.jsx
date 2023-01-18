67d091654aa82e7533040bc63c966139d9fc75e5
Wren00 <lorenwalsh00@gmail.com>
2023-01-18 15:01:39 +0000

    Adding further styling to Details page and adding error handling

--- src/app.jsx
+++ src/app.jsx
@@ -3,11 +3,22 @@ import { createRoot } from "react-dom/client";
 import SearchParams from "./SearchParams";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Details from "./Details";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            staleTime: Infinity,
+            cacheTime: Infinity,
+        },
+    },
+});
 
 const App = () => {
   return (
       <div>
           <BrowserRouter>
+              <QueryClientProvider client={queryClient}>
               <header>
                   <Link to="/">Adopt Me!</Link>
               </header>;
@@ -15,6 +26,7 @@ const App = () => {
                   <Route path="/details/:id" element={<Details />} />
                   <Route path="/" element={<SearchParams />} />
               </Routes>
+              </QueryClientProvider>
           </BrowserRouter>;
       </div>
   );
